54f00f4535f5df12510be8ae75338dd9e8d65d53
Droogans <droogans@gmail.com>
2016-02-16 15:21:49 -0600

    Solve rosalind problems from the command line

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # rosalind
 My Rosalind Solutions
+
+To solve a new problem, grab the text file from the problem online, and make a note of the filename.
+
+> rosalind_ini3.txt
+
+```
+wpoR2sT77ULTxwXaC9DNJKsM2jSUFlaDvhySPNATyhO92brubeDVnRXnx6YYzS88UHolodactylusbDN4pAeMJiGTF0IWa0u8VSWrBcJXmog5JI0kDVyklRxUxkCZihLQ1aJwqFhemilasiuswhlfQ1AxRYA6H3QNFTcNnNiWfn7cuE.
+65 76 135 144
+```
+
+Create a new file that has the same name as the text file you just downloaded.
+
+> rosalind_ini3.py
+
+In that file, create a function called `main`.
+
+```py
+def main(input):
+    """ I am the input from the text file. Split, slice, do whatever to solve the problem. """
+    pass
+```
+
+The answer should post directly to the command line.
+
+```
+$> cd ~/code/py/rosalind
+$> python solve.py rosalind_ini3 --downloads_directory=~/Desktop
+```
+
+My rosalind text files downloaded to my desktop, so I pass in that argument here.
---
+++ solve.py
@@ -0,0 +1,51 @@
+import os.path
+
+def grab_input(rosalind_problem_name, downloads_directory):
+    """
+    Opens a text file representing rosalind input to solve a problem.
+    Pass in the name of the current rosalind problem name to look it up
+    in your downloads directory, where rosalind text files typically end up.
+
+    >>> grab_input('rosalind_ini3.txt')
+    wpoR2sT77ULTxwXaC9DNJKsM2jSUFlaDvhySPNATyhO92brubeDVnRXnx6YYzS88UHolodact
+    ylusbDN4pAeMJiGTF0IWa0u8VSWrBcJXmog5JI0kDVyklRxUxkCZihLQ1aJwqFhemilasiusw
+    hlfQ1AxRYA6H3QNFTcNnNiWfn7cuE.
+    65 76 135 144
+    """
+    if downloads_directory is None:
+        downloads_directory = '~/Downloads'
+    downloads_directory = os.path.expanduser(downloads_directory)
+    return open(os.path.join(downloads_directory, rosalind_problem_name), 'r').read()
+
+if __name__ == '__main__':
+    # you've called this from the command line
+    import argparse
+    import importlib
+
+    parser = argparse.ArgumentParser(
+        description=(
+            '''
+            Solve a rosalind problem. Make sure you name your python file that contains
+            the `main` function the same thing as the text file you downloaded from rosalind.
+            '''
+        )
+    )
+
+    parser.add_argument(
+        'problem_name',
+        action='store',
+        help='the name of the file you downloaded for this problem, minus the extension'
+    )
+
+    parser.add_argument(
+        '--downloads-directory',
+        dest='downloads_directory',
+        action='store',
+        help='the directory where your rosalind downloads end up'
+    )
+
+    args = parser.parse_args()
+    rosalind_solver = importlib.import_module(args.problem_name)
+    rosalind_input = grab_input(args.problem_name + '.txt', args.downloads_directory)
+
+    print rosalind_solver.main(rosalind_input)
