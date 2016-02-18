f1211ec18ad3d47ccb9ada3c7b84f711f24e1202
rbcasperson <casperson.ryan@gmail.com>
2016-02-18 17:49:13 -0500

    Fixed an accidental merge mistake
    My local version was not updated, so I tried to fix ini5, which we already fixed and I merged (from not my computer).
    I am now bringing it back to what was already there, which is what we want. Lesson learned.

--- rosalind_ini5.py
+++ rosalind_ini5.py
@@ -1,4 +1,4 @@
 def main(input):
     lines = input.split("\n")
-    results = [lines[i] for i in range(len(lines)) if i % 2]       
-        return "\n".join([line for line in results])
+    size = len(lines) + 1		 +    results = [lines[i] for i in range(len(lines)) if i % 2]       
+        return '\n'.join(lines[1:size:2]).strip()
