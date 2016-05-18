37754bba14cf4b849e641327bd7fae56c054b0b3
rbcasperson <casperson.ryan@gmail.com>
2016-05-18 12:02:47 -0400

    Fix flake8 issues and spruce up

--- rosalind_lcsm.py
+++ rosalind_lcsm.py
@@ -1,12 +1,6 @@
-def is_in_each(sub, dna_strings):
-    for dna_string in dna_strings:
-        if not sub in dna_string:
-            return False
-    return True
-
 def main(data):
     lines = [text.replace("\n", "") for text in data.split(">")]
-    dna_strings = [line[13:]for line in lines[1:]]
+    dna_strings = [line[13:] for line in lines[1:]]
     first = dna_strings.pop(0)
     lcs = ''
 
@@ -14,7 +8,7 @@ def main(data):
         sub_length = 2
         while start + sub_length <= len(first):
             test_sub = first[start:start + sub_length]
-            if is_in_each(test_sub, dna_strings):
+            if all(test_sub in dna_string for dna_string in dna_strings):
                 if len(test_sub) > len(lcs):
                     lcs = test_sub
                 sub_length += 1
--- tests/test_rosalind_lcsm.py
+++ tests/test_rosalind_lcsm.py
@@ -1,5 +1,5 @@
-from nose.tools import assert_equal
 from rosalind.rosalind_lcsm import main
+
 
 def test_main():
     sample_dataset = open('./tests/sample_data/lcsm_data.txt').read()
